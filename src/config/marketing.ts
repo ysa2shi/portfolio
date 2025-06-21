746dc8a22ebd2848f25552a9b2708560c013528e
ysa2shi <satoshi.1109.work@gmail.com>
2025-06-22 02:44:14 +0900

    修正

--- src/config/marketing.ts
+++ src/config/marketing.ts
@@ -1,21 +1,23 @@
-import type { MarketingConfig } from "../types"
+import type { MarketingConfig } from "../../types"
 
 export const marketingConfig: MarketingConfig = {
   pageInfo: {
     home: {
       title: "Home",
       href: "/",
-      description: "Sharing what I've built and learned along the way."
+      description: "Sharing what I've built and learned along the way.",
     },
     projects: {
       title: "Projects",
       href: "/projects",
-      description: "I would like to introduce some projects I’ve recently completed."
+      description:
+        "I would like to introduce some projects I’ve recently completed.",
     },
     articles: {
       title: "Articles",
       href: "/articles",
-      description: "An overview of my recent thoughts and developments within the technology field."
+      description:
+        "An overview of my recent thoughts and developments within the technology field.",
     },
   },
 }
